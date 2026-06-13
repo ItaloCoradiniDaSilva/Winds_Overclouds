d0de995f452413ef1febb32b7cdf71c25f1a3a6d
Italo Coradini Da Silva <italocoradinidasilva@gmail.com>
2026-06-12 22:43:45 -0300

    Alteração em todas as paginas HTML. Criação da função de filtragem. estilização da pagina conta, e voos.

--- perfil.js
+++ perfil.js
@@ -3,23 +3,25 @@ const contadora = JSON.parse(localStorage.getItem("contadora"));
 const outSaidaDados = document.getElementById("saidaDados");
 
 if (contadora == 1) {
-    const rgUsuario = vetUsuario[4];
-    const dataUsuario = vetUsuario[5];
+
     const nomeUsuario = vetUsuario[0];
     const idadeUsuario = vetUsuario[1];
     const emailUsuario = vetUsuario[2];
     const senhaUsuario = vetUsuario[3];
+    const rgUsuario = vetUsuario[4];
+    const dataUsuario = vetUsuario[5];
 
-
-    outSaidaDados.innerHTML = "Seu nome: " + nomeUsuario + "<br>" +
-        "Sua idade: " + idadeUsuario + "<br>" +
-        "Seu email: " + emailUsuario + "<br>" +
-        "Sua senha: " + senhaUsuario + "<br>" +
-        "Seu RG: " + rgUsuario + "<br>" +
-        "Sua data de nascimento: " + dataUsuario;
+    outSaidaDados.innerHTML =
+        "<strong>Nome:</strong> " + nomeUsuario + "<br><br>" +
+        "<strong>Idade:</strong> " + idadeUsuario + "<br><br>" +
+        "<strong>E-mail:</strong> " + emailUsuario + "<br><br>" +
+        "<strong>Senha:</strong> " + senhaUsuario + "<br><br>" +
+        "<strong>RG:</strong> " + rgUsuario + "<br><br>" +
+        "<strong>Data de Nascimento:</strong> " + dataUsuario;
 
 } else {
-    outSaidaDados.innerHTML = "<h2>Faça o cadastro</h2>";
-    
+
+    outSaidaDados.innerHTML =
+        "<h2>Nenhum usuário encontrado</h2>" +
+        "<p>Faça seu cadastro para visualizar seus dados.</p>";
 }
-console.log(nomeUsuario);
